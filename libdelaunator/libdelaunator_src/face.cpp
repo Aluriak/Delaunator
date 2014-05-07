98beb6acd0fff5b3e9538bcbb8720c4bb05b9b31
Aluriak <lucas.bourneuf@laposte.net>
2014-05-07 23:10:09 +0200

    other way for manage epsilon error on circumcircleContainCoords method

--- libdelaunator/libdelaunator_src/face.cpp
+++ libdelaunator/libdelaunator_src/face.cpp
@@ -13,7 +13,6 @@ Face::Face(Edge* e, bool visible) : visible(visible), edge(e) {
                 this->computeInternalValues();
                 e->setLeftFace(this);
         }
-
 }
 
 Face::~Face() {
@@ -66,9 +65,9 @@ bool Face::circumcircleContainCoords(Coordinates p0) const {
 
         // Algorithm found here: https://en.wikipedia.org/wiki/Delaunay_triangulation#Algorithms
         // matrix:
-        // A B C
-        // D E F
-        // G H I
+        // A B C        0 1 2
+        // D E F        3 4 5
+        // G H I        6 7 8
         // Trnaslate in:
         // p1.x - p0.x          p1.y - p0.y             (p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y)
         // p2.x - p0.x          p2.y - p0.y             (p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y)
@@ -76,6 +75,22 @@ bool Face::circumcircleContainCoords(Coordinates p0) const {
         // If determinant of this matrix is < 0, p is in circumcircle of t
         // determinant: AEI + BFG + CDH - AFH - BDI - CEG
         // p0: tested 
+        //float val[9] = {
+                //p1.x - p0.x,    p1.y - p0.y,    (p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y),
+                //p2.x - p0.x,    p2.y - p0.y,    (p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y),
+                //p3.x - p0.x,    p3.y - p0.y,    (p3.x*p3.x-p0.x*p0.x) + (p3.y*p3.y-p0.y*p0.y)
+        //};
+        //for(float v : val) 
+                 //if(fabs(v) < EPSILON) v = 0.;
+
+
+        //float d = (    val[0] * val[4] * val[8] // AEI
+                     //+ val[1] * val[5] * val[6] // BFG
+                     //+ val[2] * val[3] * val[7] // CDH
+                     //- val[0] * val[5] * val[7] // AFH
+                     //- val[1] * val[3] * val[8] // BDI
+                     //- val[2] * val[4] * val[6] // CEG
+                  //);
         float d = (    (p1.x - p0.x) * (p2.y - p0.y) * ((p3.x*p3.x-p0.x*p0.x) + (p3.y*p3.y-p0.y*p0.y)) // AEI
                      + (p1.y - p0.y) * ((p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y)) * (p3.x - p0.x) // BFG
                      + ((p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y)) * (p2.x - p0.x) * (p3.y - p0.y) // CDH
@@ -87,7 +102,7 @@ bool Face::circumcircleContainCoords(Coordinates p0) const {
 #if DEBUG
         assert(fabs(d) > -1); // According to Murphy's law, it will happens.
 #endif
-        return d < (-EPSILON);
+        return d < (-2. * EPSILON); // must be certain that p0 is NOT ON circumcircle.
 
 }
 
