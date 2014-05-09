0355d8cfd2c4c4933386f28daab34af7f939b9b1
Aluriak <lucas.bourneuf@laposte.net>
2014-05-10 00:04:30 +0200

    Replace accessors on x and y Vertex field

--- libdelaunator/libdelaunator_src/face.cpp
+++ libdelaunator/libdelaunator_src/face.cpp
@@ -31,8 +31,8 @@ void Face::computeInternalValues() {
         Vertex *p1 = this->getP1(), *p2 = this->getP2(), *p3 = this->getP3();
         
         // Deduce coordinates of Centroid.
-        this->centroid.x = (p1->x + p2->x + p3->x) / 3.;
-        this->centroid.y = (p1->y + p2->y + p3->y) / 3.;
+        this->centroid.setX((p1->x() + p2->x() + p3->x()) / 3.);
+        this->centroid.setY((p1->y() + p2->y() + p3->y()) / 3.);
 }
 
 
@@ -68,36 +68,42 @@ bool Face::circumcircleContainCoords(Coordinates p0) const {
         // A B C        0 1 2
         // D E F        3 4 5
         // G H I        6 7 8
-        // Trnaslate in:
-        // p1.x - p0.x          p1.y - p0.y             (p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y)
-        // p2.x - p0.x          p2.y - p0.y             (p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y)
-        // p3.x - p0.x          p3.y - p0.y             (p3.x*p3.x-p0.x*p0.x) + (p3.y*p3.y-p0.y*p0.y)
+        // Translate in:
+        // p1.x() - p0.x()      p1.y() - p0.y()     (p1.x()*p1.x()-p0.x()*p0.x()) + (p1.y()*p1.y()-p0.y()*p0.y())
+        // p2.x() - p0.x()      p2.y() - p0.y()     (p2.x()*p2.x()-p0.x()*p0.x()) + (p2.y()*p2.y()-p0.y()*p0.y())
+        // p3.x() - p0.x()      p3.y() - p0.y()     (p3.x()*p3.x()-p0.x()*p0.x()) + (p3.y()*p3.y()-p0.y()*p0.y())
         // If determinant of this matrix is < 0, p is in circumcircle of t
         // determinant: AEI + BFG + CDH - AFH - BDI - CEG
-        // p0: tested 
-        //float val[9] = {
-                //p1.x - p0.x,    p1.y - p0.y,    (p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y),
-                //p2.x - p0.x,    p2.y - p0.y,    (p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y),
-                //p3.x - p0.x,    p3.y - p0.y,    (p3.x*p3.x-p0.x*p0.x) + (p3.y*p3.y-p0.y*p0.y)
-        //};
-        //for(float v : val) 
-                 //if(fabs(v) < EPSILON) v = 0.;
+        float val[9] = {
+              p1.x() - p0.x(),   p1.y() - p0.y(),   (p1.x()*p1.x()-p0.x()*p0.x()) + (p1.y()*p1.y()-p0.y()*p0.y()),
+              p2.x() - p0.x(),   p2.y() - p0.y(),   (p2.x()*p2.x()-p0.x()*p0.x()) + (p2.y()*p2.y()-p0.y()*p0.y()),
+              p3.x() - p0.x(),   p3.y() - p0.y(),   (p3.x()*p3.x()-p0.x()*p0.x()) + (p3.y()*p3.y()-p0.y()*p0.y())
+        };
+        for(float v : val) 
+                 if(fabs(v) < EPSILON) v = 0.;
 
 
-        //float d = (    val[0] * val[4] * val[8] // AEI
-                     //+ val[1] * val[5] * val[6] // BFG
-                     //+ val[2] * val[3] * val[7] // CDH
-                     //- val[0] * val[5] * val[7] // AFH
-                     //- val[1] * val[3] * val[8] // BDI
-                     //- val[2] * val[4] * val[6] // CEG
-                  //);
-        float d = (    (p1.x - p0.x) * (p2.y - p0.y) * ((p3.x*p3.x-p0.x*p0.x) + (p3.y*p3.y-p0.y*p0.y)) // AEI
-                     + (p1.y - p0.y) * ((p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y)) * (p3.x - p0.x) // BFG
-                     + ((p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y)) * (p2.x - p0.x) * (p3.y - p0.y) // CDH
-                     - (p1.x - p0.x) * ((p2.x*p2.x-p0.x*p0.x) + (p2.y*p2.y-p0.y*p0.y)) * (p3.y - p0.y) // AFH
-                     - (p1.y - p0.y) * (p2.x - p0.x) * ((p3.x*p3.x-p0.x*p0.x) + (p3.y*p3.y-p0.y*p0.y)) // BDI
-                     - ((p1.x*p1.x-p0.x*p0.x) + (p1.y*p1.y-p0.y*p0.y)) * (p2.y - p0.y) * (p3.x - p0.x) // CEG
+        float d = (    val[0] * val[4] * val[8] // AEI
+                     + val[1] * val[5] * val[6] // BFG
+                     + val[2] * val[3] * val[7] // CDH
+                     - val[0] * val[5] * val[7] // AFH
+                     - val[1] * val[3] * val[8] // BDI
+                     - val[2] * val[4] * val[6] // CEG
                   );
+        //float d = (    
+        //// AEI
+          //(p1.x()-p0.x()) * (p2.y()-p0.y()) * ((p3.x()*p3.x()-p0.x()*p0.x()) + (p3.y()*p3.y()-p0.y()*p0.y())) 
+        //// BFG
+        //+ (p1.y()-p0.y()) * ((p2.x()*p2.x()-p0.x()*p0.x()) + (p2.y()*p2.y()-p0.y()*p0.y())) * (p3.x()-p0.x()) 
+        //// CDH
+        //+ ((p1.x()*p1.x()-p0.x()*p0.x()) + (p1.y()*p1.y()-p0.y()*p0.y())) * (p2.x()-p0.x()) * (p3.y()-p0.y()) 
+        //// AFH
+        //- (p1.x()-p0.x()) * ((p2.x()*p2.x()-p0.x()*p0.x()) + (p2.y()*p2.y()-p0.y()*p0.y())) * (p3.y()-p0.y()) 
+        //// BDI
+        //- (p1.y()-p0.y()) * (p2.x()-p0.x()) * ((p3.x()*p3.x()-p0.x()*p0.x()) + (p3.y()*p3.y()-p0.y()*p0.y())) 
+        //// CEG
+        //- ((p1.x()*p1.x()-p0.x()*p0.x()) + (p1.y()*p1.y()-p0.y()*p0.y())) * (p2.y()-p0.y()) * (p3.x()-p0.x()) 
+        //);
 
 #if DEBUG
         assert(fabs(d) > -1); // According to Murphy's law, it will happens.
