4d6d16b563c08b9bcadf944e56cfb5f802f0d2e7
Aluriak <lucas.bourneuf@laposte.net>
2014-06-05 23:29:33 +0200

    rewrite for comprehensibilty and pypi acceptation

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@
 #########################
 # IMPORTS               #
 #########################
-from setuptools import setup, find_packages, Extension, distutils
+from setuptools import setup, find_packages, Extension
+from distutils import sysconfig 
 from info import __version__, __name__
 import os, re
 
 
 
 # Delete unwanted flags for C compilation
-d = distutils.sysconfig.get_config_vars()
+# find a better way to do that is necessary
+d = sysconfig.get_config_vars()
 for k, v in d.items():
     if str(v).find('-Wstrict-prototypes') != -1:
-        #v = distutils.sysconfig.get_config_vars()[k] = str(v).replace('-Wstrict-prototypes', '')
         v = d[k] = str(v).replace('-Wstrict-prototypes', '')
     if str(v).find('-DNDEBUG') != -1:
-        #v = distutils.sysconfig.get_config_vars()[k] = str(v).replace('-DNDEBUG', '')
         v = d[k] = str(v).replace('-DNDEBUG', '')
 
 
@@ -28,21 +28,19 @@ for k, v in d.items():
 #########################
 # EXTENSIONS            #
 #########################
-# MACROS IN C CODE
-defines = [
-    ('DEBUG', 1),
-    ('DEULAUNAY_FINDER_INITIAL_RANDOM', None),
-    #('DEULAUNAY_FINDER_INITIAL_FIRST', None),
-    #('DEULAUNAY_FINDER_INITIAL_MIDDLE', None),
-]
 # SWIG COMPILATION
 swig_extension = Extension(
     'delaunator.libdelaunator_src._libdelaunator',
     sources = [('delaunator/libdelaunator_src/'+_) for _ in os.listdir('delaunator/libdelaunator_src/') if os.path.splitext(_)[1] == '.cpp'] + 
                 ['delaunator/libdelaunator_src/libdelaunator.i'],
-    #swig_opts = ['-modern', '-c++'],
     swig_opts = ['-c++'],
-    define_macros = defines,
+    define_macros = [
+        ('DEBUG', 1),
+        # choose your finder mode here !
+        ('DEULAUNAY_FINDER_INITIAL_RANDOM', None),
+        #('DEULAUNAY_FINDER_INITIAL_FIRST', None),
+        #('DEULAUNAY_FINDER_INITIAL_MIDDLE', None),
+    ],
     optional = False,
     extra_compile_args = [
         '-std=c++11',
@@ -77,27 +75,14 @@ setup(
         "Programming Language :: C",
         "Programming Language :: C++",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Allegro CL",
         "Programming Language :: C#",
-        "Programming Language :: CFFI",
-        "Programming Language :: CLISP",
-        "Programming Language :: Chicken",
-        "Programming Language :: D",
-        "Programming Language :: Go",
-        "Programming Language :: Guile",
         "Programming Language :: Java",
-        "Programming Language :: Lua",
-        "Programming Language :: Modula-3",
-        "Programming Language :: Mzscheme",
-        "Programming Language :: OCAML",
-        "Programming Language :: Octave",
+        "Programming Language :: OCaml",
+        "Programming Language :: Other",
         "Programming Language :: Perl",
         "Programming Language :: PHP",
         "Programming Language :: Python",
-        "Programming Language :: R",
-        "Programming Language :: Ruby",
         "Programming Language :: Tcl",
-        "Programming Language :: UFFI",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ]
