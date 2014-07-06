ecf8ff394e5bed77ef60ecf80bd905af7b557f83
Aluriak <lucas.bourneuf@laposte.net>
2014-07-07 00:10:23 +0200

    choose the random mode 'cause its show the use of args (and its funny too)

--- pydelaunator/main.py
+++ pydelaunator/main.py
@@ -22,7 +22,7 @@ from random import randint
 # MAIN                  #
 #########################
 if __name__ == '__main__':
-    dt = Delaunator(0,600,0,600)
+    dt = Delaunator(0,600,0,600, Delaunator.FINDER_INITIAL_RANDOM)
 
     gui = Delinterface(dt)
     
