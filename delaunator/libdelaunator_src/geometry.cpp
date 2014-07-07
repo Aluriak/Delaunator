0666e2ec4d12b69365e23b966742e0fce1c66672
Aluriak <lucas.bourneuf@laposte.net>
2014-07-07 11:33:15 +0200

    move doc to source file

--- delaunator/libdelaunator_src/geometry.cpp
+++ delaunator/libdelaunator_src/geometry.cpp
@@ -6,6 +6,13 @@
 /***************************************************
  * POINT IN CIRCUMCIRCLE OF 
  ***************************************************/
+/**
+ * @param p1 Coordinates of a point of triangle
+ * @param p2 Coordinates of a point of triangle
+ * @param p3 Coordinates of a point of triangle
+ * @param p0 Coordinates of tested point
+ * @return true iff tested point is in circumcircle of triangle composed by p1, p2 and p3.
+ */
 bool geometry::pointInCircumcircleOf(Coordinates p1, Coordinates p2, Coordinates p3, Coordinates p0) {
         // Algorithm found here: https://en.wikipedia.org/wiki/Delaunay_triangulation#Algorithms
         // matrix:
@@ -50,6 +57,12 @@ bool geometry::pointInCircumcircleOf(Coordinates p1, Coordinates p2, Coordinates
 /***************************************************
  * CIRCUMCENTER OF
  ***************************************************/
+/**
+ * @param A Coordinates of a point of triangle
+ * @param B Coordinates of a point of triangle
+ * @param C Coordinates of a point of triangle
+ * @return C Coordinates of triangle's circumcenter
+ */
 Coordinates geometry::circumcenterOf(Coordinates A, Coordinates B, Coordinates C) {
         // Source: wikipedia.org/wiki/Circumscribed_circle#Cartesian_coordinates
         float D = 2*(A.x()*(B.y() - C.y()) + B.x()*(C.y() - A.y()) + C.x()*(A.y() - B.y()));
@@ -91,8 +104,12 @@ inline float collideAt_distanceSquarePointToSegment(float x1, float y1, float x2
         else
                 return (x - x2)*(x - x2) + (y - y2)*(y - y2);
 }
-/*
- * POINT IN TRIANGLE
+/**
+ * @param p1 Coordinates of a point of triangle
+ * @param p2 Coordinates of a point of triangle
+ * @param p3 Coordinates of a point of triangle
+ * @param p0 Coordinates of tested point
+ * @return true iff tested point is in triangle composed by p1, p2 and p3.
  */
 bool geometry::pointInTriangle(Coordinates p1, Coordinates p2, Coordinates p3, Coordinates p0) {
         bool collide = false;
@@ -130,6 +147,16 @@ bool geometry::pointInTriangle(Coordinates p1, Coordinates p2, Coordinates p3, C
 /***************************************************
  * DISTANCE BETWEEN POINT AND SEGMENT
  ***************************************************/
+/**
+ * @param x1 coordinate in x for first point of segment
+ * @param y1 coordinate in y for first point of segment
+ * @param y2 coordinate in y for second point of segment
+ * @param x2 coordinate in x for second point of segment
+ * @param px coordinate in x of tested point
+ * @param py coordinate in y of tested point
+ * @return shorter square distance between segment and point
+ * @warning return value is square of distance, not real distance
+ */
 float geometry::distanceBetweenPointAndSegment(float x1, float x2, float y1, float y2, float px, float py) {
         // Algorithm found on http://totologic.blogspot.fr/2014/01/accurate-point-in-triangle-test.html
         float p1_p2_square_dist = (x2-x1) * (x2-x1) + (y2-y1) * (y2-y1);
@@ -157,6 +184,13 @@ float geometry::distanceBetweenPointAndSegment(float x1, float x2, float y1, flo
 /***************************************************
  * INTERSECTION OF LINES
  ***************************************************/
+/**
+ * @param A Coordinates of a line point
+ * @param B Coordinates of another line point
+ * @param C Coordinates of a line point
+ * @param D Coordinates of another line point
+ * @return true if lines are parallels
+ */
 bool geometry::parallelsLines(Coordinates A, Coordinates B, Coordinates C, Coordinates D) {
         // source: https://en.wikipedia.org/wiki/Line-line_intersection#Mathematics
         return fabs((A.x()-B.x()) * (C.y()-D.y())  -  (A.y()-B.y()) * (C.x()-D.x())) < EPSILON;
@@ -172,6 +206,14 @@ bool geometry::parallelsLines(Coordinates A, Coordinates B, Coordinates C, Coord
 /***************************************************
  * INTERSECTION OF LINES
  ***************************************************/
+/**
+ * @param A Coordinates of a line point
+ * @param B Coordinates of another line point
+ * @param C Coordinates of a line point
+ * @param D Coordinates of another line point
+ * @return Coordinates of intersection of the lines
+ * @warning if lines are parallels, result is false
+ */
 Coordinates geometry::intersectionOfLines(Coordinates A, Coordinates B, Coordinates C, Coordinates D) {
         assert(!geometry::parallelsLines(A,B,C,D));
         Coordinates intersec;
@@ -193,6 +235,13 @@ Coordinates geometry::intersectionOfLines(Coordinates A, Coordinates B, Coordina
 /***************************************************
  * COLLISION BETWEEN SEGMENT AND LINE
  ***************************************************/
+/**
+ * @param O Coordinates of a segment point
+ * @param P Coordinates of another segment point
+ * @param A Coordinates of a line point
+ * @param B Coordinates of another line point
+ * @return true if [OP] segment cross with (AB) line
+ */
 bool geometry::collisionBetweenSegmentAndLine(Coordinates O, Coordinates P, Coordinates A, Coordinates B) {
         // 0,1    1,0   0,0     2,2
         float AB_x = B.x() - A.x();  // 2
@@ -214,6 +263,13 @@ bool geometry::collisionBetweenSegmentAndLine(Coordinates O, Coordinates P, Coor
 /***************************************************
  * COLLISION BETWEEN SEGMENT AND SEGMENT
  ***************************************************/
+/**
+ * @param A Coordinates of a first segment point
+ * @param B Coordinates of another first segment point
+ * @param C Coordinates of a second segment point
+ * @param D Coordinates of another second segment point
+ * @return true if [AB] segment cross with [CD] segment
+ */
 bool geometry::collisionBetweenSegmentAndSegment(Coordinates A, Coordinates B, Coordinates C, Coordinates D) {
         return collisionBetweenSegmentAndLine(A, B, C, D) && collisionBetweenSegmentAndLine(C, D, A, B);
 }
@@ -228,6 +284,13 @@ bool geometry::collisionBetweenSegmentAndSegment(Coordinates A, Coordinates B, C
 /***************************************************
  * COLLISION BETWEEN LINE AND CIRCLE
  ***************************************************/
+/**
+ * @param A Coordinates of a line point
+ * @param B Coordinates of another line point
+ * @param C Coordinates of circle's center 
+ * @param radius of the circle
+ * @return true if circle collide with (AB) line
+ */
 bool geometry::collisionBetweenLineAndCircle(Coordinates A, Coordinates B, Coordinates C, float radius) {
         float u_x = B.x() - A.x();
         float u_y = B.y() - A.y();
@@ -248,6 +311,12 @@ bool geometry::collisionBetweenLineAndCircle(Coordinates A, Coordinates B, Coord
 /***************************************************
  * POINT IN CIRCLE
  ***************************************************/
+/**
+ * @param A Coordinates of tested point
+ * @param C Coordinates of circle's center 
+ * @param radius of the circle
+ * @return true if tested point is in circle
+ */
 bool geometry::pointInCircle(Coordinates A, Coordinates C, float radius) {
         return (A.x()-C.x())*(A.x()-C.x()) + (A.y()-C.y())*(A.y()-C.y()) > radius*radius;
 }
@@ -262,6 +331,13 @@ bool geometry::pointInCircle(Coordinates A, Coordinates C, float radius) {
 /***************************************************
  * COLLISION BETWEEN SEGMENT AND CIRCLE
  ***************************************************/
+/**
+ * @param A Coordinates of segment point
+ * @param B Coordinates of segment point
+ * @param C Coordinates of circle's center 
+ * @param radius of the circle
+ * @return true if circle collide with [AB] segment
+ */
 bool geometry::collisionBetweenSegmentAndCircle(Coordinates A, Coordinates B, Coordinates C, float radius) {
         bool collision = false;
         if(geometry::collisionBetweenLineAndCircle(A, B, C, radius)) {
@@ -291,6 +367,12 @@ bool geometry::collisionBetweenSegmentAndCircle(Coordinates A, Coordinates B, Co
 /***************************************************
  * PROJECTION OF POINT ON LINE
  ***************************************************/
+/**
+ * @param A a point on the line
+ * @param B another point on the line
+ * @param C the evaluated point
+ * @return the projection of evaluated point on the line
+ */
 Coordinates geometry::projectionOfPointOnLine(Coordinates A, Coordinates B, Coordinates C) {
         float u_x  = B.x() - A.x(); 
         float u_y  = B.y() - A.y(); 
@@ -311,6 +393,16 @@ Coordinates geometry::projectionOfPointOnLine(Coordinates A, Coordinates B, Coor
 /***************************************************
  * INTERSECTIONS BETWEEN LINE AND CIRCLE
  ***************************************************/
+/**
+ * @param A Coordinates of a point on the line
+ * @param B Coordinates of another point on the line
+ * @param C Coordinates of center of circle
+ * @param radius of the circle 
+ * @param S1 reference to Coordinates of the first find point of intersection between cricle and line
+ * @param S2 reference to Coordinates of the second find point
+ * @return number of solution, 0, 1 or 2
+ * @note if return 0, S1 and S2 are not modified, if 1, S1 only is modifed, else, both modified
+ */
 unsigned int geometry::intersectionsBetweenLineAndCircle(Coordinates A, Coordinates B, Coordinates C, 
                 float radius, Coordinates* S1, Coordinates* S2) {
 #if DEBUG
@@ -366,6 +458,14 @@ unsigned int geometry::intersectionsBetweenLineAndCircle(Coordinates A, Coordina
 /***************************************************
  * COEF AND ORDN OF LINE
  ***************************************************/
+/**
+ * @param A Coordinates of a point on the line
+ * @param B Coordinates of another point on the line
+ * @param coef ref of float, modified
+ * @param ordn ref of float, modified
+ * @return false if line is vertical, else true
+ * @warning if line is vertical, ordn only is modifed and represent the offset in x axis
+ */
 bool geometry::coeffAndOrdnOfLine(Coordinates A, Coordinates B, float* coef, float* ordn) {
 #if DEBUG
         assert(coef != NULL);
--- delaunator/libdelaunator_src/geometry.h
+++ delaunator/libdelaunator_src/geometry.h
@@ -24,152 +24,33 @@
  * Geometry is a namespace that define lots of useful geometry functions.
  */
 namespace geometry {
-
-
-        /**
-         * @param p1 Coordinates of a point of triangle
-         * @param p2 Coordinates of a point of triangle
-         * @param p3 Coordinates of a point of triangle
-         * @param p0 Coordinates of tested point
-         * @return true iff tested point is in circumcircle of triangle composed by p1, p2 and p3.
-         */
         bool pointInCircumcircleOf(Coordinates p1, Coordinates p2, Coordinates p3, Coordinates p0);
 
-
-        /**
-         * @param A Coordinates of a point of triangle
-         * @param B Coordinates of a point of triangle
-         * @param C Coordinates of a point of triangle
-         * @return C Coordinates of triangle's circumcenter
-         */
         Coordinates circumcenterOf(Coordinates A, Coordinates B, Coordinates C);
-
-
-        /**
-         * @param p1 Coordinates of a point of triangle
-         * @param p2 Coordinates of a point of triangle
-         * @param p3 Coordinates of a point of triangle
-         * @param p0 Coordinates of tested point
-         * @return true iff tested point is in triangle composed by p1, p2 and p3.
-         */
+        
         bool pointInTriangle(Coordinates p1, Coordinates p2, Coordinates p3, Coordinates p0);
-
-
-        /**
-         * @param x1 coordinate in x for first point of segment
-         * @param y1 coordinate in y for first point of segment
-         * @param y2 coordinate in y for second point of segment
-         * @param x2 coordinate in x for second point of segment
-         * @param px coordinate in x of tested point
-         * @param py coordinate in y of tested point
-         * @return shorter square distance between segment and point
-         * @warning return value is square of distance, not real distance
-         */
+        
         float distanceBetweenPointAndSegment(float x1, float x2, float y1, float y2, float px, float py);
-
-
-        /**
-         * @param A Coordinates of a line point
-         * @param B Coordinates of another line point
-         * @param C Coordinates of a line point
-         * @param D Coordinates of another line point
-         * @return true if lines are parallels
-         */
+        
         bool parallelsLines(Coordinates A, Coordinates B, Coordinates C, Coordinates D);
-
-
-        /**
-         * @param A Coordinates of a line point
-         * @param B Coordinates of another line point
-         * @param C Coordinates of a line point
-         * @param D Coordinates of another line point
-         * @return Coordinates of intersection of the lines
-         * @warning if lines are parallels, result is false
-         */
+        
         Coordinates intersectionOfLines(Coordinates A, Coordinates B, Coordinates C, Coordinates D);
-
-
-        /**
-         * @param O Coordinates of a segment point
-         * @param P Coordinates of another segment point
-         * @param A Coordinates of a line point
-         * @param B Coordinates of another line point
-         * @return true if [OP] segment cross with (AB) line
-         */
+        
         bool collisionBetweenSegmentAndLine(Coordinates O, Coordinates P, Coordinates A, Coordinates B);
-
-
-        /**
-         * @param A Coordinates of a first segment point
-         * @param B Coordinates of another first segment point
-         * @param C Coordinates of a second segment point
-         * @param D Coordinates of another second segment point
-         * @return true if [AB] segment cross with [CD] segment
-         */
+        
         bool collisionBetweenSegmentAndSegment(Coordinates A, Coordinates B, Coordinates C, Coordinates D);
-
-
-        /**
-         * @param A Coordinates of a line point
-         * @param B Coordinates of another line point
-         * @param C Coordinates of circle's center 
-         * @param radius of the circle
-         * @return true if circle collide with (AB) line
-         */
+        
         bool collisionBetweenLineAndCircle(Coordinates A, Coordinates B, Coordinates C, float radius);
-
-
-        /**
-         * @param A Coordinates of tested point
-         * @param C Coordinates of circle's center 
-         * @param radius of the circle
-         * @return true if tested point is in circle
-         */
+        
         bool pointInCircle(Coordinates A, Coordinates C, float radius);
-
-
-        /**
-         * @param A Coordinates of segment point
-         * @param B Coordinates of segment point
-         * @param C Coordinates of circle's center 
-         * @param radius of the circle
-         * @return true if circle collide with [AB] segment
-         */
+        
         bool collisionBetweenSegmentAndCircle(Coordinates A, Coordinates B, Coordinates C, float radius);
-
-
-        /**
-         * @param A a point on the line
-         * @param B another point on the line
-         * @param C the evaluated point
-         * @return the projection of evaluated point on the line
-         */
+        
         Coordinates projectionOfPointOnLine(Coordinates A, Coordinates B, Coordinates C);
-
-
-        /**
-         * @param A Coordinates of a point on the line
-         * @param B Coordinates of another point on the line
-         * @param C Coordinates of center of circle
-         * @param radius of the circle 
-         * @param S1 reference to Coordinates of the first find point of intersection between cricle and line
-         * @param S2 reference to Coordinates of the second find point
-         * @return number of solution, 0, 1 or 2
-         * @note if return 0, S1 and S2 are not modified, if 1, S1 only is modifed, else, both modified
-         */
+        
         unsigned int intersectionsBetweenLineAndCircle(Coordinates A, Coordinates B, Coordinates C, 
                         float radius, Coordinates* S1, Coordinates* S2);
-
-
-
-        /**
-         * @param A Coordinates of a point on the line
-         * @param B Coordinates of another point on the line
-         * @param coef ref of float, modified
-         * @param ordn ref of float, modified
-         * @return false if line is vertical, else true
-         * @warning if line is vertical, ordn only is modifed and represent the offset in x axis
-         */
+        
         bool coeffAndOrdnOfLine(Coordinates A, Coordinates B, float* coef, float* ordn);
 }
 
