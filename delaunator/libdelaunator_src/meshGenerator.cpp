09e0ba5e0867577413bbdd1a67f9c8b466edeba3
Aluriak <lucas.bourneuf@laposte.net>
2014-12-12 01:05:46 +0100

    automatic mesh generation

---
+++ delaunator/libdelaunator_src/meshGenerator.cpp
@@ -0,0 +1,41 @@
+#include "meshGenerator.h"
+
+
+
+
+
+/******************************************************************
+ * FROM DENSITY FUNCTION
+ ******************************************************************/
+/**
+ * Creat a Delaunator instance with many points, placed between given bounds at given step from each other, 
+ * according to given density function. The density function must return a value in [0;1]
+ * @param fDensity a function pointer for a float, float -> float function
+ * @param bound_min the minimal coordinate in all dimension
+ * @param bound_max the maximal coordinate in all dimension
+ * @param step the minimal distance between two points
+ * @return address of a new and initialized Delaunator object
+ */
+Delaunator* MeshGenerator::fromDensityFunction(float (*fDensity)(float, float), 
+                unsigned int bound_minx, unsigned int bound_maxx, 
+                unsigned int bound_miny, unsigned int bound_maxy, float step) {
+// INIT
+        Delaunator* dt = new Delaunator(bound_minx, bound_maxx, bound_miny, bound_maxy);
+        float ci = bound_minx, cj = bound_miny;
+// TREATMENT
+        for(; ci <= bound_maxx; ci += step) {
+                for(; cj <= bound_maxy; cj += step) {
+                        // get random number in [0;1]
+                        if(rand() <= (fDensity(ci, cj) * 100)) {
+                                dt->addVirtualVertex(Coordinates(ci, cj));
+                        }
+                }
+        }
+// END
+        return dt;
+}
+
+
+
+
+
---
+++ delaunator/libdelaunator_src/meshGenerator.h
@@ -0,0 +1,41 @@
+#ifndef MESH_GENERATOR_H_INCLUDED
+#define MESH_GENERATOR_H_INCLUDED
+
+
+
+/*
+ * LIBRARIES
+ */
+// LOCAL MODULES
+#include "commons.h"
+#include "delaunator.h"
+
+
+
+/*
+ * DEFINES
+ */
+
+
+
+/*
+ * PREDECLARATIONS
+ */
+
+
+
+
+
+
+/**
+ * Methods for automatic generation of Delaunator intances
+ */
+namespace MeshGenerator {
+    Delaunator* fromDensityFunction(float (*)(float, float), unsigned int, unsigned int, 
+                                    unsigned int, unsigned int, float);
+
+}
+
+
+
+#endif
